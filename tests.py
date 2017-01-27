95c5aa91c5c6232fd81731f2afb2de697b5663b7
Cody Heimberger <certifiedloud@gmail.com>
2017-01-27 06:53:48 -0700

    more stubbing

--- tests.py
+++ tests.py
@@ -22,11 +22,39 @@ class TimeSpeedDistance(unittest.TestCase):
         distance = self.e6b.time(1, 60)
         self.assertEqual(60, distance)   
 
+class Fuel(unittest.TestCase):
+    '''Fuel calculations'''
+
+    def test_fuel_endurance(self):
+        '''Test fuel endurance calculation'''
+        pass
+
+    def test_average_fuel_consumption(self):
+        pass
+
+    def test_fuel_capacity(self):
+        pass
+
 class Wind(unittest.TestCase):
     '''Test wind calculations'''
 
     def test_wind_correction_angle(self):
         '''Test wind correction angle calculation'''
+        pass
+
+class Conversionts(unittest.TestCase):
+    '''Unit conversions'''
+
+    def test_cel_to_fahr(self):
+        pass
+
+    def test_fahr_to_cel(self):
+        pass
+
+    def test_nautical_to_statute(self):
+        pass
+
+    def test_statute_to_nautical(self):
         pass
 
 if __name__ == '__main__':
